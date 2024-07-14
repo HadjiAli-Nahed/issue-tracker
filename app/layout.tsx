01463e3889d86c1945fa9aaab9282512dac8793e
zhack <zhacky@gmail.com>
2024-07-15 06:22:31 +0800

    005 Add Markdown Editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
 		<Theme appearance='light' accentColor='violet' scaling='110%'>
 			<NavBar />
 			<main className='p-5'>{children}</main>
-			<ThemePanel />
 		</Theme>
 		</body>
 		</html>
