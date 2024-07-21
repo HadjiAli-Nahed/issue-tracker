2c931f27b6237c99db7701d8fb761ba3b58bd272
zhack <zhacky@gmail.com>
2024-07-22 00:31:55 +0800

    029 Improve user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,19 +3,24 @@ import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Spinner } from "@/app/components";
 
 const DeleteIssueButton = ({issueId}: { issueId: number }) => {
 	const router = useRouter();
 	const deleteIssue = async () => {
 		try {
+			setIsDeleting(true)
 			await axios.delete('/api/issues/' + issueId);
 			router.push('/issues')
 			router.refresh()
 		} catch (error) {
+			setIsDeleting(false)
 			setError(true);
 		}
 	}
 	const [error, setError] = useState(false)
+	const [isDeleting, setIsDeleting] = useState(false)
+
 	return (
 		<>
 			<AlertDialog.Root>
@@ -31,8 +36,8 @@ const DeleteIssueButton = ({issueId}: { issueId: number }) => {
 							<Button variant='soft' color='gray'>Cancel</Button>
 						</AlertDialog.Cancel>
 						<AlertDialog.Action>
-							<Button color='red' onClick={deleteIssue}
-							>Delete Issue</Button>
+							<Button color='red' onClick={deleteIssue} disabled={isDeleting}
+							>Delete Issue {isDeleting && <Spinner />}</Button>
 						</AlertDialog.Action>
 					</Flex>
 				</AlertDialog.Content>
