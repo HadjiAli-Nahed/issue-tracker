e110e29cec0dc93e8f880fad6086faaad7e8a82d
zhack <zhacky@gmail.com>
2024-07-22 00:41:48 +0800

    030 Remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -11,11 +11,11 @@ const NavBar = () => {
 
 	const links = [
 		{label: 'Dashboard', href: '/'},
-		{label: 'Issues', href: '/issues'}
+		{label: 'Issues', href: '/issues/list'}
 	]
 	return (
 		<nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-			<Link href='/'><AiFillBug/></Link>
+			<Link href='/'><AiFillBug /></Link>
 			<ul className='flex space-x-6'>
 				{links.map(link => <Link key={link.href}
 										 className={classnames({
--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const IssueActions = () => {
 	return (
 		<div className='mb-5'>
-			<Button><Link href='../issues/new/'>New Issue</Link></Button>
+			<Button><Link href='/issues/new'>New Issue</Link></Button>
 		</div>
 	)
 }
