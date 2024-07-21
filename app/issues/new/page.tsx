5a1ba61325e0d59389764b0901e4a6648a5afcb7
zhack <zhacky@gmail.com>
2024-07-21 23:48:28 +0800

    024 Improve loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,11 @@
 import dynamic from "next/dynamic";
+import IssueFormSkeleton from "../components/IssueFormSkeleton";
 
 const IssueForm = dynamic(
 	() => import('@/app/issues/components/IssueForm'),
 	{
 		ssr: false,
+		loading: () => <IssueFormSkeleton />
 	}
 );
 
